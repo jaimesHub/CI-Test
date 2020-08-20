cf90bfd32c6c9296251e3ebe986d7ee5db7962a4
jaimesHub <56724481+jaimesHub@users.noreply.github.com>
2020-08-20 15:57:23 +0700

    Update Bai_2.js

--- Problem Solving/Bai_2.js
+++ Problem Solving/Bai_2.js
@@ -1,23 +1,28 @@
 function merge2String(str1, str2) {
     var temp = str1.concat(str2);
     var str_result = '';
-    if (temp.length / 2 === 0) {
+    if (str1.length === str2.length) {
         for (var i = 0; i < str1.length; i++) {
-            str_result += temp[i] + temp[i + 3] // le
+            str_result += temp[i] + temp[i + temp.length / 2] // le
         }
     } else {
-        for (var i = 0; i < (temp.length - 1) / 2; i++) {
-            str_result += temp[i] + temp[i + 3];
-        }
+
         if (str1.length > str2.length) {
+            for (var i = 0; i < (temp.length - 1) / 2; i++) {
+                str_result += temp[i] + temp[i + (temp.length - 1) / 2 + 1];
+            }
             str_result += str1[str1.length - 1];
         } else if (str1.length < str2.length) {
-            str_result += str1[str2.length - 1];
+            for (var i = 0; i < (temp.length - 1) / 2; i++) {
+                str_result += temp[i] + temp[i + (temp.length - 1) / 2];
+            }
+            str_result += str2[str2.length - 1];
         }
     }
     return str_result;
-
-
-
-
 }
+
+merge2String('abc', 'xyz');
+
+// merge2String('abcd','xyz');
+// merge2String('abc','xyzt');
